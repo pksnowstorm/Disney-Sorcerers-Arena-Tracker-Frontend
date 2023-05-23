0cb81dbac88b54bea41927f9335b95d63dd57605
pksnowstorm <enguyen21@gmail.com>
2023-05-23 07:42:35 -0500

    add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react'
+const Footer = () => (
+      <footer className="footer">
+        <p>Copyright 2023 Disney Sorcerer's Arena Tracker. All Rights Reserved</p>
+      </footer>
+    );
+  
+  export default Footer;
